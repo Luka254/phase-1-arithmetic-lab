9be74b78cbe6731951c2565f6f1294a12c1ebcf8
Luke <luke.kazungu@student.moringaschool.com>
2022-08-22 00:02:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 9;
+let num4 = 5;
+
+const random = Math.floor(Math.random() *10) + 1;
+const multiply = num1*num2;
+const mod = num3 % num4
+const max = Math.max(1, 2, 3, 4, 20);
 
 // Write your code here
